e7ee27175a6dff3b6fdc4e3c01fde3ddd4e4c02f
Stefano Romanelli <stefano.romanelli@deel.com>
2025-09-25 09:28:02 +0200

    update docusaurus config with disable trailing slash and setting main as the branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -29,8 +29,8 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'stefano-romanelli-Deel', // Usually your GitHub org/user name.
   projectName: 'dev-docs-as-code', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
-  trailingSlash: false,
+  deploymentBranch: 'main',
+  // trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
